6f7fa5ac908a0c73e0fdf76aba66543de06a3d5a
codys-wright <acodywright@gmail.com>
2025-06-21 21:47:44 -0700

    reverting back to lazyvim

--- nixos-modules/user/app/nvim.back/lua/plugins/colorschema.lua
+++ nixos-modules/user/app/nvim.back/lua/plugins/colorschema.lua
@@ -14,8 +14,8 @@ return {
     config = function()
       require("transparent").setup({
         extra_groups = {
-          "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-          "NvimTreeNormal" -- NvimTree
+          -- "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+          -- "NvimTreeNormal" -- NvimTree
         },
       })
     end,

6c554530057e914bfc04670d533ba8736478fbf9
Fast Track Studio <acodywright@gmail.com>
2025-06-26 04:43:22 -0700

    {phoenix} gen 32

--- nixos-modules/user/app/nvim/nvim.nix
+++ nixos-modules/user/app/nvim/nvim.nix
@@ -36,6 +36,20 @@
           # Snacks.nvim utility collection
           utility.snacks-nvim = {
             enable = true;
+            setupOpts = {
+              bigfile = {enabled = true;};
+              dashboard = {enabled = true;};
+              explorer = {enabled = true;};
+              indent = {enabled = true;};
+              input = {enabled = true;};
+              picker = {enabled = true;};
+              notifier = {enabled = true;};
+              quickfile = {enabled = true;};
+              scope = {enabled = true;};
+              scroll = {enabled = true;};
+              statuscolumn = {enabled = true;};
+              words = {enabled = true;};
+            };
           };
 
           assistant = {

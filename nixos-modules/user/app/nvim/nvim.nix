cb2db06bf0ab49ad3c603605f716e38667311b45
Fast Track Studio <acodywright@gmail.com>
2025-06-26 04:39:35 -0700

    {phoenix} gen 31

--- nixos-modules/user/app/nvim/nvim.nix
+++ nixos-modules/user/app/nvim/nvim.nix
@@ -33,67 +33,9 @@
           # Which-key keybind helper
           binds.whichKey.enable = true;
 
-          # Custom keymaps for snacks
-          keymaps = [
-            {
-              key = "<leader><space>";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.picker.smart()<CR>";
-              silent = true;
-              desc = "Smart Find Files";
-            }
-            {
-              key = "<leader>e";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.explorer()<CR>";
-              silent = true;
-              desc = "Explorer (root dir)";
-            }
-            {
-              key = "<leader>ff";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.picker.files()<CR>";
-              silent = true;
-              desc = "Find Files";
-            }
-            {
-              key = "<leader>fg";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.picker.git_files()<CR>";
-              silent = true;
-              desc = "Find Git Files";
-            }
-            {
-              key = "<leader>fr";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.picker.recent()<CR>";
-              silent = true;
-              desc = "Recent Files";
-            }
-            {
-              key = "<leader>/";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.picker.grep()<CR>";
-              silent = true;
-              desc = "Grep";
-            }
-            {
-              key = "<leader>,";
-              mode = ["n"];
-              action = "<cmd>lua Snacks.picker.buffers()<CR>";
-              silent = true;
-              desc = "Buffers";
-            }
-          ];
-
           # Snacks.nvim utility collection
           utility.snacks-nvim = {
             enable = true;
-            setupOpts = {
-              picker = true;
-              explorer = true;
-              dashboard = true;
-            };
           };
 
           assistant = {

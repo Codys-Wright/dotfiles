49fb18cc2c59293ba23afc57583f2771516ed33b
Fast Track Studio <acodywright@gmail.com>
2025-06-26 04:18:35 -0700

    {phoenix} gen 27

--- nixos-modules/user/app/nvim/nvim.nix
+++ nixos-modules/user/app/nvim/nvim.nix
@@ -31,41 +31,10 @@
           };
 
           # Which-key keybind helper
-          binds.whichKey = {
-            enable = true;
-            setupOpts = {
-              preset = "modern";
-              win.border = "rounded";
-            };
-            register = {
-              "<leader>o" = "obsidian";
-              "<leader>oo" = "Quick Switch Notes";
-              "<leader>on" = "New Note";
-              "<leader>os" = "Search Notes";
-              "<leader>ol" = "Link to Note";
-              "<leader>od" = "Today's Note";
-              "<leader>oy" = "Yesterday's Note";
-              "<leader>ob" = "Show Backlinks";
-              "<leader>og" = "Show Tags";
-              "<leader>ow" = "Switch Workspace";
-              "<leader>of" = "Follow Link";
-              "<leader>or" = "Rename Note";
-              "<leader>oc" = "Table of Contents";
-              "<leader>op" = "Paste Image";
-              "<leader>ot" = "Insert Template";
+          binds.whichKey.enable = true;
 
-              "<leader>t" = "timer";
-              "<leader>ts" = "Start Work Timer (25min)";
-              "<leader>tb" = "Start Break Timer (5min)";
-              "<leader>tl" = "Start Long Break (15min)";
-              "<leader>tp" = "Pause Timer";
-              "<leader>tr" = "Resume Timer";
-              "<leader>te" = "Stop Timer";
-              "<leader>tt" = "Show Timer Status";
-              "<leader>tc" = "Start Pomodoro Session";
-              "<leader>th" = "Custom Timer";
-            };
-          };
+          # Snacks.nvim utility collection
+          utility.snacks-nvim.enable = true;
         };
       }
     ];

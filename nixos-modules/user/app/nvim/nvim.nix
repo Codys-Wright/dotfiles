9bceb18d6c8396429e7b930381497fc7f21d8180
Fast Track Studio <acodywright@gmail.com>
2025-06-26 04:04:36 -0700

    {phoenix} gen 22

--- nixos-modules/user/app/nvim/nvim.nix
+++ nixos-modules/user/app/nvim/nvim.nix
@@ -16,6 +16,19 @@
           statusline.lualine.enable = true;
           telescope.enable = true;
           autocomplete.nvim-cmp.enable = true;
+
+          # Mini plugins
+          mini = {
+            surround.enable = true;
+            pairs.enable = true;
+            files.enable = true;
+            pick.enable = true;
+            comment.enable = true;
+            indentscope.enable = true;
+            cursorword.enable = true;
+            hipatterns.enable = true;
+            icons.enable = true;
+          };
         };
       }
     ];
